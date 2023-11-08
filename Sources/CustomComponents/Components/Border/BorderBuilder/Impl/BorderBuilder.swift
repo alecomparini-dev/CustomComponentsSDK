bd6a3d7180473cc3cafd2692713780f39f0422b8
Comparini <alecomparini.dev@gmail.com>
2023-11-08 19:26:03 -0300

    attempt fix shadow

--- Sources/CustomComponents/Components/Border/BorderBuilder/Impl/BorderBuilder.swift
+++ Sources/CustomComponents/Components/Border/BorderBuilder/Impl/BorderBuilder.swift
@@ -44,8 +44,9 @@ open class BorderBuilder: Border {
     @discardableResult
     public func setCornerRadius(_ radius: CGFloat) -> Self {
         self.removeBorderStyleOfTextField()
-        component.clipsToBounds = true
-        component.layer.masksToBounds = true
+        if !component.hasShadow() {
+            component.layer.masksToBounds = true
+        }
         component.layer.cornerRadius = radius
         return self
     }
