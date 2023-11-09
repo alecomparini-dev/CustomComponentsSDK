716c6ae894331e7c69ddc0cd096f83264c12006c
Comparini <alecomparini.dev@gmail.com>
2023-11-09 12:29:24 -0300

    attempt gradient

--- Sources/CustomComponents/Components/Gradient/Impl/GradientBuilder.swift
+++ Sources/CustomComponents/Components/Gradient/Impl/GradientBuilder.swift
@@ -93,24 +93,27 @@ open class GradientBuilder: Gradient {
 //  MARK: - APPLY GRADIENT
     @discardableResult
     public func apply() -> Self {
-        removeGradient()
-        
-        DispatchQueue.main.async { [weak self] in
-            guard let self, let component else {return}
-
-        }
-        
         guard let component else {return self}
+        removeGradient()
         setGradientOnComponent()
-        
-        gradient.frame = component.bounds
+        gradient.removeAllAnimations()
         gradient.cornerRadius = component.layer.cornerRadius
         gradient.maskedCorners = component.layer.maskedCorners
+        
+        DispatchQueue.main.async { [weak self] in
+            guard let self else {return}
 
-        if !isAxial && gradient.endPoint == CGPointZero {
-            let endY = component.frame.size.width / component.frame.size.height / 2
-            gradient.endPoint = CGPoint(x: 0, y: endY)
+            gradient.frame = component.bounds
+
+            if !isAxial && gradient.endPoint == CGPointZero {
+                let endY = component.frame.size.width / component.frame.size.height / 2
+                gradient.endPoint = CGPoint(x: 0, y: endY)
+            }
+
         }
+        
+        
+        
 
         
         return self
