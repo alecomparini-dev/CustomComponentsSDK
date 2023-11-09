ef0f0d7081cfa388a39291ea5e0b1c0917901a49
Comparini <alecomparini.dev@gmail.com>
2023-11-09 08:59:52 -0300

    tests

--- Sources/CustomComponents/Handlers/Extensions/UIView+Extension.swift
+++ Sources/CustomComponents/Handlers/Extensions/UIView+Extension.swift
@@ -46,12 +46,15 @@ public extension UIView {
         }
     }
     
-    func countShadows() -> Int {
-        var count = self.layer.sublayers?.filter({ $0.shadowOpacity > 0 }).count ?? 0
-        //TODO: Pode ocorrer um possivel problema no shadow applyLayer, fazer testes
-        return (self.layer.shadowOpacity > 0) ? count + 1 : count
+    func countShadows() -> (shadowLayer: Int, shadowComponent: Bool) {
+        let countShadowLayer = self.layer.sublayers?.filter({ $0.shadowOpacity > 0 }).count ?? 0
+        let countShadowComponent = self.layer.shadowOpacity > 0
+        return (shadowLayer: countShadowLayer, shadowComponent: countShadowComponent)
     }
     
-    func hasShadow() -> Bool { return countShadows() > 0 }
+    func hasShadow() -> Bool {
+        let hasShadow = countShadows()
+        return (hasShadow.shadowLayer > 0 || hasShadow.shadowComponent)
+    }
 }
 
