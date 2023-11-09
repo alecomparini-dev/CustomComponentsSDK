f7bfb371f17e4eaa416ad72980fed23069137b67
Comparini <alecomparini.dev@gmail.com>
2023-11-09 08:30:07 -0300

    tests

--- Sources/CustomComponents/Components/Border/BorderBuilder/Impl/BorderBuilder.swift
+++ Sources/CustomComponents/Components/Border/BorderBuilder/Impl/BorderBuilder.swift
@@ -45,7 +45,7 @@ open class BorderBuilder: Border {
     @discardableResult
     public func setCornerRadius(_ radius: CGFloat) -> Self {
         self.removeBorderStyleOfTextField()
-        component?.layer.masksToBounds = true
+//        component?.layer.masksToBounds = true
 //        component.clipsToBounds = true
         component.layer.cornerRadius = radius
         return self
