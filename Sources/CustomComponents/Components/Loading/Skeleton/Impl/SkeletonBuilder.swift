13c457bae5c6f0f3d5ccc81c9aa0d6324e2e97e1
Comparini <alecomparini.dev@gmail.com>
2023-11-09 23:03:24 -0300

    skeleton tests

--- Sources/CustomComponents/Components/Loading/Skeleton/Impl/SkeletonBuilder.swift
+++ Sources/CustomComponents/Components/Loading/Skeleton/Impl/SkeletonBuilder.swift
@@ -91,7 +91,7 @@ open class SkeletonBuilder: Skeleton {
     private func configFrame() {
         guard let component else {return}
         skeletonView.add(insideTo: component.baseView.superview ?? UIView())
-        skeletonView.get.frame = component.baseView.bounds
+        skeletonView.get.bounds = component.baseView.bounds
         skeletonView.get.layer.cornerRadius = component.baseView.layer.cornerRadius
     }
     
