231c012193264f5e12735dc7ed1237924f1e46b0
Comparini <alecomparini.dev@gmail.com>
2023-11-10 09:59:23 -0300

    skeleton tests

--- Sources/CustomComponents/Components/Loading/Skeleton/Impl/SkeletonBuilder.swift
+++ Sources/CustomComponents/Components/Loading/Skeleton/Impl/SkeletonBuilder.swift
@@ -180,15 +180,18 @@ open class SkeletonBuilder: Skeleton {
     private func stopAnimation() {
         skeletonView.get.updateConstraintsIfNeeded()
         skeletonView.get.layoutIfNeeded()
-        if let transitionDuration {
-            UIView.animate(withDuration: transitionDuration, delay: .zero, animations: { [weak self] in
-                self?.skeletonView.get.alpha = 0
-            }, completion: { [weak self] _ in
-                guard let self else {return}
-                hide()
-            })
-            return
-        }
+//        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1, execute: {
+            if let transitionDuration {
+                UIView.animate(withDuration: transitionDuration, delay: 1, animations: { [weak self] in
+                    self?.skeletonView.get.alpha = 0
+                }, completion: { [weak self] _ in
+                    guard let self else {return}
+                    hide()
+                })
+                return
+            }
+//        })
+        
         hide()
     }
     
