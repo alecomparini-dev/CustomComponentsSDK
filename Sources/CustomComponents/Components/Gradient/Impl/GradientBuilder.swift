cbfd276132bd85920a2f14fffead275edb58ca4a
Comparini <alecomparini.dev@gmail.com>
2023-11-16 08:32:04 -0300

    freememory on shadowBuilder

--- Sources/CustomComponents/Components/Gradient/Impl/GradientBuilder.swift
+++ Sources/CustomComponents/Components/Gradient/Impl/GradientBuilder.swift
@@ -95,7 +95,14 @@ open class GradientBuilder: Gradient {
     public func apply() -> Self {
         removeGradient()
         applyMainThread()
+        freeMemory()
         return self
+    }
+    
+    private func freeMemory() {
+        DispatchQueue.main.async {
+            self.component = nil
+        }
     }
     
 //  MARK: - REMOVE GRADIENT
@@ -178,11 +185,11 @@ open class GradientBuilder: Gradient {
 //  MARK: - APPLY
 
     private func applyMainThread() {
-        DispatchQueue.main.async {
-            self.setGradientOnComponent()
-            self.setFrame()
-            self.calculateEndPoint()
-            self.component = nil
+        DispatchQueue.main.async { [weak self] in
+            guard let self else {return}
+            setGradientOnComponent()
+            setFrame()
+            calculateEndPoint()
         }
     }
     
