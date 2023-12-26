ffc3fed381a293c8577369f472d0c92ac2ef574d
Comparini <alecomparini.dev@gmail.com>
2023-12-26 10:53:55 -0300

    style

--- Sources/CustomComponents/Components/BaseBuilder/Extensions/UIView+BaseBuilder+Extension.swift
+++ Sources/CustomComponents/Components/BaseBuilder/Extensions/UIView+BaseBuilder+Extension.swift
@@ -7,56 +7,56 @@ extension UIView {
     
     @discardableResult
     public func setFrame(_ frame: CGRect?) -> Self {
-        guard let frame else {return self}
+        guard let frame else { return self }
         self.frame = frame
         return self
     }
     
     @discardableResult
     public func setBackgroundColor(hexColor: String?) -> Self {
-        guard let hexColor else {return self}
+        guard let hexColor else { return self }
         self.backgroundColor = UIColor.HEX(hexColor)
         return self
     }
     
     @discardableResult
     public func setBackgroundColor(named: String?) -> Self {
-        guard let named else {return self}
+        guard let named else { return self }
         self.backgroundColor = UIColor(named: named)
         return self
     }
     
     @discardableResult
     public func setBackgroundColor(_ color: UIColor?) -> Self {
-        guard let color else {return self}
+        guard let color else { return self }
         self.backgroundColor = color
         return self
     }
     
     @discardableResult
     func setIsUserInteractionEnabled(_ interactionEnabled: Bool?) -> Self {
-        guard let interactionEnabled else {return self}
+        guard let interactionEnabled else { return self }
         self.isUserInteractionEnabled = interactionEnabled
         return self
     }
     
     @discardableResult
     public func setOpacity(_ opacity: Float?) -> Self {
-        guard let opacity else {return self}
+        guard let opacity else { return self }
         self.layer.opacity = opacity
         return self
     }
     
     @discardableResult
     public func setAlpha(_ alpha: CGFloat?) -> Self {
-        guard let alpha else {return self}
+        guard let alpha else { return self }
         self.alpha = alpha
         return self
     }
    
     @discardableResult
     public func setHidden(_ hide: Bool?) -> Self {
-        guard let hide else {return self}
+        guard let hide else { return self }
         self.isHidden = hide
         return self
     }
