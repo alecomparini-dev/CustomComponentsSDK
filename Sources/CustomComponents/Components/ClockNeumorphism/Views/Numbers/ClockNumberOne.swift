6ebc7937414f7a2e45712f4d9d14e6aa745d9da3
Comparini <alecomparini.dev@gmail.com>
2024-02-16 18:22:13 -0300

    create ClockNumberOne

--- Sources/CustomComponents/Components/ClockNeumorphism/Views/Numbers/ClockNumberOne.swift
+++ Sources/CustomComponents/Components/ClockNeumorphism/Views/Numbers/ClockNumberOne.swift
@@ -3,48 +3,9 @@
 
 import Foundation
 
-
-class ClockNumber: ViewBuilder {
-    let strokeModel: StrokeModel
-    
-    init(strokeModel: StrokeModel = StrokeModel()) {
-        self.strokeModel = strokeModel
-        super.init()
-        configure()
-    }
-    
-    lazy var clockNumberContainer: ClockNumberContainer = {
-        let comp = ClockNumberContainer()
-            .setConstraints { build in
-                build
-                    .setPin.equalToSuperView
-            }
-        return comp
-    }()
-    
-    
-//  MARK: - PRIVATE AREA
-    private func configure() {
-        addElement()
-        configConstraints()
-    }
-    
-    private func addElement() {
-        clockNumberContainer.add(insideTo: self.get)
-    }
-    
-    private func configConstraints() {
-        clockNumberContainer.applyConstraint()
-    }
-    
-    
-}
-
-
-
 class ClockNumberOne: ClockNumber  {
         
-    override init(strokeModel: StrokeModel) {
+    override init(strokeModel: StrokeModel = StrokeModel()) {
         super.init(strokeModel: strokeModel)
         configure()
     }
