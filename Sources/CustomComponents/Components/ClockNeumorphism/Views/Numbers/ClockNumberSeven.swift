dfaaafe5437ee47c5ed944905c86ac2b2f27b70d
Comparini <alecomparini.dev@gmail.com>
2024-02-16 21:26:09 -0300

    fix

---
+++ Sources/CustomComponents/Components/ClockNeumorphism/Views/Numbers/ClockNumberSeven.swift
@@ -0,0 +1,59 @@
+//  Created by Alessandro Comparini on 16/02/24.
+//
+
+import Foundation
+
+class ClockNumberSeven: ClockNumber  {
+        
+    override init(strokeModel: StrokeModel = StrokeModel()) {
+        super.init(strokeModel: strokeModel)
+        configure()
+    }
+    
+    
+    //  MARK: - LEFT
+    lazy var leftTopStroke: Stroke = {
+        let comp = createStroke()
+        return comp
+    }()
+
+    
+    //  MARK: - RIGHT
+    lazy var rightTopStroke: Stroke = {
+        let comp = createStroke()
+        return comp
+    }()
+
+    lazy var rightBottomStroke: Stroke = {
+        let comp = createStroke()
+        return comp
+    }()
+
+    
+//  MARK: - PRIVATE AREA
+    private func configure() {
+        addElement()
+        configConstraints()
+    }
+    
+    private func addElement() {
+        leftTopStroke.add(insideTo: clockNumberContainer.leftColumn.topView.get)
+        rightTopStroke.add(insideTo: clockNumberContainer.rightColumn.topView.get)
+        rightBottomStroke.add(insideTo: clockNumberContainer.rightColumn.bottomView.get)
+    }
+    
+    private func configConstraints() {
+        leftTopStroke.applyConstraint()
+        rightTopStroke.applyConstraint()
+        rightBottomStroke.applyConstraint()
+    }
+    
+    private func createStroke() -> Stroke {
+        return Stroke(strokeModel: strokeModel)
+            .setConstraints { build in
+                build
+                    .setPin.equalToSuperView
+            }
+    }
+    
+}
