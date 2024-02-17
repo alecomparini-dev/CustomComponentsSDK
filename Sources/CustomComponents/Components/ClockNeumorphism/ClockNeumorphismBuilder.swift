db71fee0f994776cbc25aa78781072cdc6979198
Comparini <alecomparini.dev@gmail.com>
2024-02-16 23:00:25 -0300

    create number five

--- Sources/CustomComponents/Components/ClockNeumorphism/ClockNeumorphismBuilder.swift
+++ Sources/CustomComponents/Components/ClockNeumorphism/ClockNeumorphismBuilder.swift
@@ -144,7 +144,7 @@ public class ClockNeumorphismBuilder: ViewBuilder {
     private func updateClock() {
         removeSubviews()
         let hourView1 = ClockNumbers(number: getHour(firstPosition: true), weight: weight)
-        let hourView2 = ClockNumberOne()
+        let hourView2 = ClockNumberThree()
         let minuteView1 = ClockNumberTwo()
         let minuteView2 = ClockNumberFive()
         hourView1.add(insideTo: stackHours.get)
