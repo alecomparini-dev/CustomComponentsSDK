45bbe55340221bf5769dc61218fc0b58c6823415
Comparini <alecomparini.dev@gmail.com>
2024-03-02 16:51:48 -0300

    fix

--- Sources/CustomComponents/Components/AutoLayout/_Flows/StartConstraintsFlow/StartPositionConstraintFlow.swift
+++ Sources/CustomComponents/Components/AutoLayout/_Flows/StartConstraintsFlow/StartPositionConstraintFlow.swift
@@ -49,23 +49,11 @@ public class StartPositionConstraintFlow<T> {
     
     //  MARK: - CONSTRAINTS
     
-    public func equalTo(_ relationElement: Any, _ toAttribute: T? = nil, _ constant: CGFloat? = nil) -> StartAutoLayout  {
+    public func equalTo(_ relationElement: Any, _ toAttribute: T, _ constant: CGFloat = 0) -> StartAutoLayout  {
         guard let toAttribute = toAttribute as? ConstraintsAttributeProtocol else {return startAutoLayout }
-        Constraints(startAutoLayout).set(relationBy: .equal, relationElement: relationElement, toAttribute: toAttribute.toConstraintsAttribute(), constant: constant ?? 0)
+        Constraints(startAutoLayout).set(relationBy: .equal, relationElement: relationElement, toAttribute: toAttribute.toConstraintsAttribute(), constant: constant)
         return startAutoLayout
     }
-    
-    public func equalTo(_ relationElement: Any, constant: CGFloat? = nil) -> StartAutoLayout  {
-        Constraints(startAutoLayout).set(relationBy: .equal, relationElement: relationElement, constant: constant ?? 0)
-        return startAutoLayout
-    }
-
-    public func equalTo(_ relationElement: Any) -> StartAutoLayout  {
-        Constraints(startAutoLayout).set(relationBy: .equal, relationElement: relationElement)
-        return startAutoLayout
-    }
-    
-    
         
     public func greaterThanOrEqualTo(_ relationElement: Any, _ toAttribute: T, _ constant: CGFloat = 0) -> StartAutoLayout  {
         guard let toAttribute = toAttribute as? ConstraintsAttributeProtocol else {return startAutoLayout }
