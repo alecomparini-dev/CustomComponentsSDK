871d6f66ef50649a0e6741876d421c0d0da252b2
Comparini <alecomparini.dev@gmail.com>
2024-03-13 14:54:40 -0300

    improvements on textfields

--- Sources/CustomComponents/Components/TextField/Actions/ClearButtonModeBuilder/Impl/ClearButtonModeBuilder.swift
+++ Sources/CustomComponents/Components/TextField/Actions/ClearButtonModeBuilder/Impl/ClearButtonModeBuilder.swift
@@ -5,7 +5,7 @@ import UIKit
 
 public class ClearButtonModeBuilder: ClearButtonMode {
     
-    private var size: CGSize = CGSize(width: 20, height: 20)
+    private var size: CGSize = CGSize(width: 24, height: 24)
     private var systemName: String = K.Images.xCircleFill
     
     
