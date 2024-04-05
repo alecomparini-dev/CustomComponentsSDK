0455c97a422deee35f91d13cd9140b18cdbb044c
Comparini <alecomparini.dev@gmail.com>
2024-04-05 12:31:47 -0300

    improvements dropdown

--- Sources/CustomComponents/Components/DropdownMenu/DropdownMenuItemsBuilder/DropdownMenuItemsBuilder.swift
+++ Sources/CustomComponents/Components/DropdownMenu/DropdownMenuItemsBuilder/DropdownMenuItemsBuilder.swift
@@ -5,20 +5,30 @@ import UIKit
 
 public class DropdownMenuItemsBuilder {
     
-    private(set) var section: ViewBuilder?
-    private(set) var rows: [ViewBuilder] = []
+    private var section: ViewBuilder?
+    private var rows: [ViewBuilder] = []
+    private var items: [ViewBuilder : [ViewBuilder]] = [:]
     
     public init() {  }
     
+    
+//  MARK: - GET PROPERTIES
+    var get: [ViewBuilder : [ViewBuilder]] { items }
+    
+    
+//  MARK: - SET PROPORTIES
     @discardableResult
     public func setSection(_ sectionView:  ViewBuilder) -> Self {
-        section = sectionView
+        items.updateValue([], forKey: sectionView)
+        rows = []
         return self
     }
     
     @discardableResult
     public func setRow(_ rowView:  ViewBuilder) -> Self {
+        guard let section else {return self}
         rows.append(rowView)
+        items.updateValue(rows, forKey: section)
         return self
     }
 
