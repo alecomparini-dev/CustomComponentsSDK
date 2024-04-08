cce85480984c44a2bb54ae4a0edfbeae9ab1443b
Comparini <alecomparini.dev@gmail.com>
2024-04-08 09:32:52 -0300

    improvements dropdown

--- Sources/CustomComponents/Components/DropdownMenu/DropdownMenuItemsBuilder/DropdownMenuItemsBuilder.swift
+++ Sources/CustomComponents/Components/DropdownMenu/DropdownMenuItemsBuilder/DropdownMenuItemsBuilder.swift
@@ -17,14 +17,14 @@ public class DropdownMenuItemsBuilder {
     
 //  MARK: - SET PROPORTIES
     @discardableResult
-    public func setSection(_ sectionView:  ViewBuilder) -> Self {
+    public func setSection(_ sectionView:  BaseBuilder) -> Self {
         let item = DropdownMenuItems(section: sectionView, rows: [])
         items.append(item)
         return self
     }
     
     @discardableResult
-    public func setRow(_ rowView:  ViewBuilder) -> Self {
+    public func setRow(_ rowView:  BaseBuilder) -> Self {
         let item = items[items.count-1]
         item.rows.append(rowView)
         return self
