4b84876574f50e8e5c0e66ad2fc0e800102aa1cb
Comparini <alecomparini.dev@gmail.com>
2024-05-22 12:11:25 -0300

    fix button interaction

--- Sources/CustomComponents/Components/DropdownMenu/_Protocol/DropdownMenuEvents.swift
+++ Sources/CustomComponents/Components/DropdownMenu/_Protocol/DropdownMenuEvents.swift
@@ -6,7 +6,7 @@ import Foundation
 public protocol DropdownMenuEvents: AnyObject {
 //    func willAppearDropdowMenu()
     func didAppearDropdowMenu()
-//    func willDisappearDropdowMenu()
+    func willDisappearDropdowMenu()
     func didDisappearDropdowMenu()
 }
 
