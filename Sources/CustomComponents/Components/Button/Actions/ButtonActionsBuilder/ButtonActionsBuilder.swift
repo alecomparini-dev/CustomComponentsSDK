07a96fce1a2fd34ce3b082e5fbd25c0c27a98ff2
Comparini <alecomparini.dev@gmail.com>
2024-06-15 12:10:45 -0300

    add target on button builder

---
+++ Sources/CustomComponents/Components/Button/Actions/ButtonActionsBuilder/ButtonActionsBuilder.swift
@@ -0,0 +1,24 @@
+
+//  Created by Alessandro Comparini on 15/06/24.
+//
+
+import UIKit
+
+public class ButtonActionsBuilder: ActionBuilder {
+    
+    private weak var component: ButtonBuilder?
+    
+    public init(_ component: ButtonBuilder) {
+        self.component = component
+        super.init(component: component)
+    }
+
+//  MARK: - ACTIONS AREA
+    
+    @discardableResult
+    func setTarget(_ target: Any, _ action: Selector , _ event: UIControl.Event) -> Self {
+        component?.get.addTarget(target, action: action, for: event )
+        return self
+    }
+    
+}
