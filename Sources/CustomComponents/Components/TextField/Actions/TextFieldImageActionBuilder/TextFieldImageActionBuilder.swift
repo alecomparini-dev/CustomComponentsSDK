b4bcf265b920ccfc1c573391d22ae2ee73c89516
Comparini <alecomparini.dev@gmail.com>
2024-06-17 09:56:20 -0300

    create action to textfield, and change inheritance of the TextFieldImageBuilder of ActionBuilder to TextFieldAction

--- Sources/CustomComponents/Components/TextField/Actions/TextFieldImageActionBuilder/TextFieldImageActionBuilder.swift
+++ Sources/CustomComponents/Components/TextField/Actions/TextFieldImageActionBuilder/TextFieldImageActionBuilder.swift
@@ -5,37 +5,33 @@
 import Foundation
 
 @MainActor
-public class TextFieldImageActionBuilder: ActionBuilder {
+public class TextFieldImageActionBuilder: TextFieldAction {
     public typealias tapImageTextField = (_ image: ImageViewBuilder) -> Void
-    
-    private weak var component: TextFieldImageBuilder?
-    
-    public init(_ component: TextFieldImageBuilder) {
-        self.component = component
-        super.init(component: component)
-    }
-    
-    deinit {
-        component = nil
-    }
+
     
     @discardableResult
     public func setTapImageLeft(_ closure: @escaping tapImageTextField) -> Self {
-        guard let imageViewLeft = component?.imageViewLeft else { return self}
+        guard let textFieldImage = component as? TextFieldImageBuilder,
+            let imageViewLeft = textFieldImage.imageViewLeft else { return self}
+        
         _ = ActionBuilder(component: imageViewLeft)
             .setTap({ tapGesture in
                 closure(imageViewLeft)
             })
+        
         return self
     }
     
     
     public func setTapImageRight(_ closure: @escaping tapImageTextField) -> Self {
-        guard let imageViewRight = component?.imageViewRight else { return self}
+        guard let textFieldImage = component as? TextFieldImageBuilder,
+            let imageViewRight = textFieldImage.imageViewRight else { return self}
+        
         _ = ActionBuilder(component: imageViewRight)
             .setTap({ tapGesture in
                 closure(imageViewRight)
             })
+        
         return self
     }
    
