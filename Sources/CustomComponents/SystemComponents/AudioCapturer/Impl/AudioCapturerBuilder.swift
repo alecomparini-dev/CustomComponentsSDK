6a1d3c16f15ab423f8167af799747481492a7db0
Comparini <alecomparini.dev@gmail.com>
2025-05-02 21:12:01 -0300

    teste

--- Sources/CustomComponents/SystemComponents/AudioCapturer/Impl/AudioCapturerBuilder.swift
+++ Sources/CustomComponents/SystemComponents/AudioCapturer/Impl/AudioCapturerBuilder.swift
@@ -127,7 +127,7 @@ final public class AudioCapturerBuilder: AudioCapturer {
             
             isAudioCaptureEnable = false
             
-            try? activeAudioSession(false)
+//            try? activeAudioSession(false)
             
             DispatchQueue.main.async(execute: {
                 completion?()
