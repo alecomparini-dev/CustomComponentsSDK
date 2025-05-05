f47d5e0361b4dd01446dd827c6db7bf9267ac5d3
Comparini <alecomparini.dev@gmail.com>
2025-05-05 20:44:49 -0300

    tests

--- Sources/CustomComponents/SystemComponents/SpeechRecognition/Impl/SpeechRecognitionBuilder.swift
+++ Sources/CustomComponents/SystemComponents/SpeechRecognition/Impl/SpeechRecognitionBuilder.swift
@@ -125,7 +125,7 @@ final public class SpeechRecognitionBuilder: SpeechRecognition {
     }
     
     public func stopRecognition() {
-        speechQueue.async(execute: { [weak self] in
+        speechQueue.asyncAfter(deadline: .now() + 1, execute: { [weak self] in
             guard let self else {return}
             request?.endAudio()
             resetRecognitionTask()
