8071930c466239ede8da105e8ce0af0be70b380e
Comparini <alecomparini.dev@gmail.com>
2025-05-07 13:33:03 -0300

    tests

--- Sources/CustomComponents/SystemComponents/AudioCapturer/AudioCapturer.swift
+++ Sources/CustomComponents/SystemComponents/AudioCapturer/AudioCapturer.swift
@@ -6,5 +6,16 @@ import AVFoundation
 
 public protocol AudioCapturer {
     var delegate: AudioCapturerDelegate? { get set }
+    func checkPermission() -> AudioCapturerPermission
     
+    func requestPermission() async -> RequestPermissionStatus
+    
+    func initiateEngine() async throws
+    
+    func finalizeEngine() async throws
+    
+    func startAudioCapture() async throws
+    
+    func stopAudioCapture()
+
 }
