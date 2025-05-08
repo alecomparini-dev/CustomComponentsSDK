569289ef34c1e2ebe171650c6bb8c23cbfcfa59f
Comparini <alecomparini.dev@gmail.com>
2025-05-07 21:09:41 -0300

    tests

--- Sources/CustomComponents/SystemComponents/AudioCapturer/Impl/AudioCapturerBuilder.swift
+++ Sources/CustomComponents/SystemComponents/AudioCapturer/Impl/AudioCapturerBuilder.swift
@@ -132,6 +132,7 @@ final public class AudioCapturerBuilder: @unchecked Sendable, AudioCapturer  {
         let format = inputNode.outputFormat(forBus: 0)
 
         inputNode.installTap(onBus: 0, bufferSize: 1024, format: format) { [weak self] buffer, _ in
+            print("começou BUFFER")
             self?.outputAudioCapture(buffer)
         }
         
