46fc8c09fd326ba878c41fe51917b55f55283dca
Comparini <alecomparini.dev@gmail.com>
2025-05-30 14:16:29 -0300

    included section and row > or = zero

--- Sources/CustomComponents/UIComponents/_Helpers/Utils/Validates.swift
+++ Sources/CustomComponents/UIComponents/_Helpers/Utils/Validates.swift
@@ -7,16 +7,14 @@ import UIKit
 public struct Validates {
     
     static func percent(_ percent: CGFloat) -> Bool {
-        if !(0.0...100.0).contains(percent) {
-            return false
-        }
+        if !(0.0...100.0).contains(percent) { return false }
         return true
     }
     
     static func isValidIndexPath(_ indexPath: IndexPath, _ tableView: UITableView) -> Bool {
-        guard indexPath.section < tableView.numberOfSections else { return false }
+        guard indexPath.section >= .zero, indexPath.section < tableView.numberOfSections else { return false }
         
-        return indexPath.row < tableView.numberOfRows(inSection: indexPath.section)
+        return indexPath.row >= .zero && indexPath.row < tableView.numberOfRows(inSection: indexPath.section)
     }
     
 }
