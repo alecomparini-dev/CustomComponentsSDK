37761a7c6311204fb74439156d44e523ca09d285
Comparini <alecomparini.dev@gmail.com>
2025-07-15 12:09:00 -0300

    alterado o auto reverso para tru

--- Sources/CustomComponents/UIComponents/Animations/BaseAnimationBuilder/DTO/BaseAnimationParameters.swift
+++ Sources/CustomComponents/UIComponents/Animations/BaseAnimationBuilder/DTO/BaseAnimationParameters.swift
@@ -7,7 +7,7 @@ public struct BaseAnimationParameters {
     public var fromValue: Any? = 1.0
     public var toValue: Any? = 0
     public var duration: TimeInterval = 0.5
-    public var autoReverse: Bool = false
+    public var autoReverse: Bool = true
     public var repeatCount: Float = .infinity
     public var timingFunction = CAMediaTimingFunction(name: .linear)
     
