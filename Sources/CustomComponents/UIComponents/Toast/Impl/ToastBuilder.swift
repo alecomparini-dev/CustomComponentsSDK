6ab02e0b27ea99f4808b1373e8c2315115254cf2
Comparini <alecomparini.dev@gmail.com>
2025-07-19 17:36:31 -0300

    teste

--- Sources/CustomComponents/UIComponents/Toast/Impl/ToastBuilder.swift
+++ Sources/CustomComponents/UIComponents/Toast/Impl/ToastBuilder.swift
@@ -44,9 +44,9 @@ open class ToastBuilder: ViewBuilder, Toast {
     public func show() {
         hideTimer = Timer.scheduledTimer(timeInterval: duration, target: self, selector: #selector(selectorHide), userInfo: nil , repeats: false)
         
-        let offset: CGFloat = position == .top ? self.get.bounds.height : -self.get.bounds.height
+        let offset: CGFloat = getTargetY()
         
-        self.get.transform = CGAffineTransform(translationX: 0, y: offset)
+        self.get.transform = CGAffineTransform(translationX: 0, y: -offset)
         
         self.get.alpha = 0
         
