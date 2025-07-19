9d5ed428e2cdbe22e59d3365d891583b8e442d02
Comparini <alecomparini.dev@gmail.com>
2025-07-19 19:09:16 -0300

    teste

--- Sources/CustomComponents/UIComponents/Toast/Impl/ToastBuilder.swift
+++ Sources/CustomComponents/UIComponents/Toast/Impl/ToastBuilder.swift
@@ -47,6 +47,8 @@ open class ToastBuilder: ViewBuilder, Toast {
 //  MARK: - ACTIONS
     
     public func show() {
+        if _isShow { return }
+        
         configPositionInitial()
         
         _isShow = true
@@ -62,6 +64,63 @@ open class ToastBuilder: ViewBuilder, Toast {
             
     }
     
+    public func hide() {
+        if !_isShow { return }
+        
+        _isShow = false
+        
+        hideTimer?.invalidate()
+        
+        let targetY: CGFloat = getTargetY()
+        
+        UIView.animate(withDuration: 0.3, animations: { [weak self] in
+            
+            self?.get.frame.origin.y = targetY
+            
+        }, completion: { [weak self] _ in
+            guard let self else { return }
+            
+            self.onDismiss?()
+        })
+    }
+
+    
+    
+//  MARK: - PRIVATE AREA
+    
+    private func configure() {
+        addSwipeGesture()
+        configInitial()
+    }
+    
+    private func addSwipeGesture() {
+        let pan = UIPanGestureRecognizer(target: self, action: #selector(handlePan(_:)))
+        
+        self.get.addGestureRecognizer(pan)
+    }
+
+    private func configInitial() {
+        self.setAlpha(0.1)
+    }
+    
+    private func getTargetY() -> CGFloat {
+        let customView = self.get
+        
+        guard let window = customView.window else {
+            return 0
+        }
+        
+        let frameInWindow = customView.convert(customView.bounds, to: window)
+        
+        switch position {
+        case .top:
+            return -(frameInWindow.maxY)
+
+        case .bottom:
+            return window.bounds.height - 80
+        }
+    }
+
     private func configPositionInitial() {
         let position = getOffsetY()
         
@@ -86,60 +145,6 @@ open class ToastBuilder: ViewBuilder, Toast {
         }
         
         return (position == .top) ? height.toast : height.screen - height.toast
-    }
-    
-    public func hide() {
-        _isShow = false
-        
-        hideTimer?.invalidate()
-        
-        let targetY: CGFloat = getTargetY()
-        
-        UIView.animate(withDuration: 0.3, animations: { [weak self] in
-            
-            self?.get.frame.origin.y = targetY
-            
-        }, completion: { [weak self] _ in
-            guard let self else { return }
-            
-            self.onDismiss?()
-        })
-    }
-        
-    
-//  MARK: - PRIVATE AREA
-    
-    private func configure() {
-        addSwipeGesture()
-        configInitial()
-    }
-    
-    private func addSwipeGesture() {
-        let pan = UIPanGestureRecognizer(target: self, action: #selector(handlePan(_:)))
-        
-        self.get.addGestureRecognizer(pan)
-    }
-
-    private func configInitial() {
-        self.setAlpha(0.3)
-    }
-    
-    private func getTargetY() -> CGFloat {
-        let customView = self.get
-        
-        guard let window = customView.window else {
-            return 0
-        }
-        
-        let frameInWindow = customView.convert(customView.bounds, to: window)
-        
-        switch position {
-        case .top:
-            return -(frameInWindow.maxY)
-
-        case .bottom:
-            return window.bounds.height - 80
-        }
     }
 
     
