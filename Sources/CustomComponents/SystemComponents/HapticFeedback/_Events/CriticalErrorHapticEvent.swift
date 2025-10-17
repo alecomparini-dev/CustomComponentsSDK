8cd1fdd51705a282213527870df4b31c3287e7d3
Comparini <alecomparini.dev@gmail.com>
2025-10-17 08:53:19 -0300

    mudando os toques

--- Sources/CustomComponents/SystemComponents/HapticFeedback/_Events/CriticalErrorHapticEvent.swift
+++ Sources/CustomComponents/SystemComponents/HapticFeedback/_Events/CriticalErrorHapticEvent.swift
@@ -1,10 +1,10 @@
-//  Created by Alessandro Comparini on 16/10/25.
+//  Created by Alessandro Comparini on 17/10/25.
 //
 
 
 import CoreHaptics
 
-struct ErrorHapticEvent: HapticEvent {
+struct CriticalErrorHapticEvent: HapticEvent {
     
     func event() -> [CHHapticEvent] {
         [  CHHapticEvent(eventType: .hapticContinuous,
@@ -13,17 +13,16 @@ struct ErrorHapticEvent: HapticEvent {
                             .init(parameterID: .hapticSharpness, value: 0.2)
                          ],
                          relativeTime: 0,
-                         duration: 0.5),
+                         duration: 0.4),
            
-           CHHapticEvent(eventType: .hapticTransient,
+           CHHapticEvent(eventType: .hapticContinuous,
                          parameters: [
                             .init(parameterID: .hapticIntensity, value: 1.0),
-                            .init(parameterID: .hapticSharpness, value: 1.0)
+                            .init(parameterID: .hapticSharpness, value: 0.8)
                          ],
-                         relativeTime: 0.6),
+                         relativeTime: 0.5,
+                         duration: 0.4),
         ]
     }
     
 }
-
-
